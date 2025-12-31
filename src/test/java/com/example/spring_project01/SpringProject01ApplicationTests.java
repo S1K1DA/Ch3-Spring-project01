110e6c90d6ecdddd9ea8cc4904e7ceb64f89cb9a
EUNSIK JUNG <bamtol2100@naver.com>
2025-12-31 10:23:04 +0900

    Spring first commit

---
+++ src/test/java/com/example/spring_project01/SpringProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
